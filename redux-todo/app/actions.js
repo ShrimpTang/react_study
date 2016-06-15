c86eeeedb3bf57c12bffb8b6d8e9e89e254dbfec
tangzx <tangzx@yikeer.com>
2016-06-15 18:32:35 +0800

    redux todo 完成

--- redux-todo/app/actions.js
+++ redux-todo/app/actions.js
@@ -2,7 +2,6 @@
  * action 类型
  */
 export const ADD_TODO = 'ADD_TODO';
-export const COMPLETE_TODO = 'COMPLETE_TODO';
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 /*
@@ -22,13 +21,6 @@ export function addTodo(text) {
     return {
         type: ADD_TODO,
         text
-    }
-}
-
-export function completeTodo(index) {
-    return {
-        type: COMPLETE_TODO,
-        index
     }
 }
 
