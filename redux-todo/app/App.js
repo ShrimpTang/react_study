031050762b5e8ba138a0dad41a79c6c62793f607
tangzx <tangzx@yikeer.com>
2016-06-15 17:03:45 +0800

    redux 增加 addTodo Todo TodoList

--- redux-todo/app/App.js
+++ redux-todo/app/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store'
-class App extends  React.Component{
-    render(){
-        return(
-            <h1>Hello!</h1>
+import AddTodo from './components/AddTodo';
+import TodoList from './components/TodoList';
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <AddTodo
+                    onAddClick={text=>{
+                      console.log('add todo',text)
+                    }}/>
+                <TodoList
+                    todos={
+                        [
+                            {
+                                text:'啊数据库了',
+                                completed:false
+                            },
+                            {
+                                text:'heisidahao',
+                                completed:true
+                            }
+                        ]
+                    }
+                    onTodoClick={index=>{
+                        console.log('onTodoClick',index)
+                    }}
+                />
+            </div>
         )
     }
 }
--- redux-todo/app/store.js
+++ redux-todo/app/store.js
@@ -3,16 +3,16 @@ import {createStore} from 'redux'
 import {addTodo,setVisibilityFilter,toggleTodo,VisibilityFilters} from './actions'
 var store= createStore(todoApp);
 
-console.log(store.getState());
-let un = store.subscribe(()=>{
-    console.log(store.getState());
-})
-
-store.dispatch(addTodo('AAA'));
-store.dispatch(addTodo('BBB'));
-store.dispatch(addTodo('CCC'));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-un();
+//console.log(store.getState());
+//let un = store.subscribe(()=>{
+//    console.log(store.getState());
+//})
+//
+//store.dispatch(addTodo('AAA'));
+//store.dispatch(addTodo('BBB'));
+//store.dispatch(addTodo('CCC'));
+//store.dispatch(toggleTodo(0));
+//store.dispatch(toggleTodo(1));
+//store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+//un();
 export default store;
