77c090a4f348eca4deb3c7cc27abec328b72602b
tangzx <tangzx@yikeer.com>
2016-06-18 17:33:21 +0800

    add mock

--- react-redux-es6/src/store/configureStore.js
+++ react-redux-es6/src/store/configureStore.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux'
 import rootReducer from '../reducers'
 export default function (initialState) {
-    return createStore(rootReducer, initialState)
+    return createStore(rootReducer, initialState,window.devToolsExtension && window.devToolsExtension())
 }
