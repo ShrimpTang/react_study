848625a999e4ca2ee073b07ed0bc6c2d5e1a94ac
tangzx <tangzx@yikeer.com>
2016-06-23 18:35:41 +0800

    可以显示mui

--- react-material-ui/src/index.js
+++ react-material-ui/src/index.js
@@ -1,13 +1,17 @@
+
+require('./style/style.css')
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
-
+import AppBar from 'material-ui/AppBar';
 ReactDOM.render(
     <MuiThemeProvider>
-        <RaisedButton label="Default"/>
+        <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
     </MuiThemeProvider>,
     document.getElementById('app'));
--- react-material-ui/webpack.config.js
+++ react-material-ui/webpack.config.js
@@ -30,11 +30,11 @@ module.exports = {
                     presets: ['es2015', 'react']
                 }
             },
-            {test: /(\.css)$/, loaders: ['style', 'css']},
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-            {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+            {test: /(\.css)$/, loaders: ['style', 'css'],exclude:/node_modules/},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file',exclude:/node_modules/},
+            {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000',exclude:/node_modules/},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream',exclude:/node_modules/},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml',exclude:/node_modules/}
         ]
     }
 };
