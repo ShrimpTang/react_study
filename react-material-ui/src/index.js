5ba3a16043f50eeb3ee90705b73580dfcd89ab71
tangzx <tangzx@yikeer.com>
2016-06-23 16:37:51 +0800

    可以显示mui

--- react-material-ui/src/index.js
+++ react-material-ui/src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-ReactDOM.render(<h1>ddd</h1>, document.getElementById('app'));
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+ReactDOM.render(
+    <MuiThemeProvider>
+        <RaisedButton label="Default"/>
+    </MuiThemeProvider>,
+    document.getElementById('app'));
